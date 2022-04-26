608fbf2d1d8b7873f9a166ef2bce6e72d2b8ce1e
DESKTOP-1EP1UJ8\USER <jinhwan6047@gmail.com>
2022-04-26 13:03:11 +0900

    Add No2839_sugarFactory

---
+++ src/baekjoon_Algorithm/No2839_sugarFactory.java
@@ -0,0 +1,16 @@
+package baekjoon_Algorithm;
+
+import java.util.Scanner;
+
+public class No2839_sugarFactory {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int num;
+		
+		
+	}
+
+}
