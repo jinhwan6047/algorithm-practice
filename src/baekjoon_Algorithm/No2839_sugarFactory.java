939a105665dc392439eaaf44a25df757453c7ff6
DESKTOP-1EP1UJ8\USER <jinhwan6047@gmail.com>
2022-04-27 09:06:54 +0900

    Solve the No2839_sugarFactory

--- src/baekjoon_Algorithm/No2839_sugarFactory.java
+++ src/baekjoon_Algorithm/No2839_sugarFactory.java
@@ -8,8 +8,19 @@ public class No2839_sugarFactory {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		int num;
+		int num=-1;
 		
+		loop:
+		for(int i=0; i<(n/3)+1; i++) {
+			for(int j=0; j<(n/5)+1; j++) {
+				if((n-(5*j)-(3*i)) == 0) {
+					num = j+i;
+					break loop;
+				}
+			}
+		}
+		
+		System.out.println(num);
 		
 	}
 
